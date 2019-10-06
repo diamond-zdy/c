28237456e2deffcbd4a9a85e21fce52fd0f9d22c
diamond <1181352644@qq.com>
2019-10-06 20:47:27 +0800

    qsort的使用

---
+++ qsort use/qsort use/源.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//qsortµÄÊ¹ÓÃ
+#include <stdio.h>
+int int_cmp(const void * p1, const void * p2)
+{
+	return (*(int*)p1 > *(int*)p2);
+}
+int main()
+{
+	int arr[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 0 };
+	int i = 0;
+	qsort(arr, sizeof(arr) / sizeof(arr[0]), sizeof(int), int_cmp);
+	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		printf("%d", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
